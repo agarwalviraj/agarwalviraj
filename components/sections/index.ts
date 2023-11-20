1dabc3c6a0c12fb537b8cc1242f17e1a22e7dd9c
agarwalviraj <viraj.ag007@gmail.com>
2023-11-20 12:44:22 +0530

    feat: remove next.js code

--- components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as About } from "./About";
-export { default as ContactMe } from "./ContactMe";
-export { default as Projects } from "./Projects";
-export { default as TechStack } from "./TechStack";
-export { default as Terminal } from "./Terminal";
